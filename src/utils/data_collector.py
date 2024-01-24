8d60f75bec87e5960ecd13c3098fc37ae2a399ea
jorge-galdos <jorgegaldosjr@gmail.com>
2024-01-24 05:08:22 -0500

    refactor: update data handling to pydantic
    The data_collection file was refactored to replace the usage of the built-in json and dataclass modules with pydantic. Pydantic facilitates the serialization and deserialization of data models, and offers improved flexibility and maintainability.
    
    The conda.yml has been updated to include pydantic.

--- src/utils/data_collector.py
+++ src/utils/data_collector.py
@@ -1,24 +1,19 @@
 """Collect system data relevant to energy consumption. 
 
-The SystemDataCollector class is exported to collect system data at a point in
-time and format as JSON message for real-time data streaming.
-
-Typical usage:
-    collector = SystemDataCollector()
-    message = collector.produce_json_message()
+The SystemData class is exported to facilitate serialization and deserialization
+of system data for producing and consuming messages via Kafka. The collect_data
+function collects the data with psutil.
 """
 
 
-from dataclasses import dataclass, asdict
-import json
 from datetime import datetime
 
+from pydantic import BaseModel
 import psutil
 
 
-@dataclass
-class SystemData:
-    """Represents system data.
+class SystemData(BaseModel):
+    """Represents system data relevant to energy consumption.
 
     Attributes:
         cpu_usage: A float representing CPU usage as a percentage.
@@ -30,7 +25,8 @@ class SystemData:
         power_plugged: Indicates if the system is plugged into power.
         time_left: An integer representing estimated remaining battery life in
           seconds.
-        timestamp: A string timestamp in ISO format.
+        timestamp: A datetime object representing the date and time the data
+          was instantiated.
     """
 
     cpu_usage: float
@@ -41,62 +37,28 @@ class SystemData:
     battery_percent: int
     power_plugged: bool
     time_left: int
-    timestamp: str
+    timestamp: datetime
 
 
-class SystemDataCollector:
-    """Collects system data and produces JSON message.
+def collect_data() -> SystemData:
+    """Collects system data using psutil.
 
-    Attributes:
-        data: A SystemData instance.
+    Returns:
+        SystemData: System data instance.
     """
-
-    def __init__(self) -> None:
-        self.data = self.collect_data()
-
-    def collect_data(self) -> SystemData:
-        """Collects system data using psutils.
-
-        Returns:
-            SystemData: System data at a specific point in time.
-        """
-        # CPU usage as a percentage
-        cpu_usage = psutil.cpu_percent()
-        # CPU frequency in MHz
-        cpu_freq = psutil.cpu_freq().current
-        # Memory usage as a percentage
-        memory_usage = psutil.virtual_memory().percent
-        # Disk I/O in bytes
-        disk_io_counters = psutil.disk_io_counters()
-        disk_io = disk_io_counters.read_bytes + disk_io_counters.write_bytes
-        # Network I/O in bytes
-        net_io_counters = psutil.net_io_counters()
-        net_io = net_io_counters.bytes_sent + net_io_counters.bytes_recv
-        # Battery information
-        sensors_battery = psutil.sensors_battery()
-        battery_percent = sensors_battery.percent
-        power_plugged = sensors_battery.power_plugged
-        time_left = sensors_battery.secsleft
-        # Timestamp
-        timestamp = datetime.now().isoformat()
-        # Create and return a SystemData object
-        return SystemData(
-            cpu_usage=cpu_usage,
-            cpu_freq=cpu_freq,
-            memory_usage=memory_usage,
-            disk_io=disk_io,
-            net_io=net_io,
-            battery_percent=battery_percent,
-            power_plugged=power_plugged,
-            time_left=time_left,
-            timestamp=timestamp,
-        )
-
-    def produce_json_message(self) -> str:
-        """Produces JSON message from SystemData object using json and asdict.
-
-        Returns:
-            str: JSON message.
-        """
-        # Convert SystemData object to a JSON string
-        return json.dumps(asdict(self.data))
+    # Instantiate psutil data
+    disk_io_counters = psutil.disk_io_counters()
+    net_io_counters = psutil.net_io_counters()
+    sensors_battery = psutil.sensors_battery()
+    # Create and return SystemData object
+    return SystemData(
+        cpu_usage=psutil.cpu_percent(),
+        cpu_freq=psutil.cpu_freq().current,
+        memory_usage=psutil.virtual_memory().percent,
+        disk_io=disk_io_counters.read_bytes + disk_io_counters.write_bytes,
+        net_io=net_io_counters.bytes_sent + net_io_counters.bytes_recv,
+        battery_percent=sensors_battery.percent,
+        power_plugged=sensors_battery.power_plugged,
+        time_left=sensors_battery.secsleft,
+        timestamp=datetime.now(),
+    )
