fa5acfcb4cc628980cbc66d08c875f5819399de8
jorge-galdos <jorgegaldosjr@gmail.com>
2024-02-04 20:45:11 -0500

    feat: add pydantic model for command to send to produce service endpoint
    The `orchestrate` app will send commands to different endpoint services in the AI system. By leveraging pydantic classes, these commands can be validated before being sent via the Kafka broker.
    
    The docker-compose.yml has been updated to create the `commands.produce` topic at start up.
    
    The `raw.data` topic has been renamed `data.raw` for hierarchical naming.

---
+++ orchestrate/command_models/produce.py
@@ -0,0 +1,19 @@
+"""Pydantic command models for Produce service. 
+"""
+
+from pydantic import BaseModel, PositiveInt
+
+
+class StartProducingCommand(BaseModel):
+    """Command model for `start_producing` command.
+
+    Attributes:
+        name (str): Command name.
+        interval (PositiveInt): Interval at which to produce system data.
+        max_messages (PositiveInt): Optional flag to limit the total number of
+          messages produced.
+    """
+
+    name: str
+    interval: PositiveInt
+    max_messages: PositiveInt
