2235a27de4d97f8cb0572c8aef413de064c2beef
jorge-galdos <jorgegaldosjr@gmail.com>
2024-02-05 23:53:06 -0500

    feat(service-orchestrate): add faust app for sending commands to the produce service
    Faust apps will facilitate the development of a CLI app, the orchestration service, which will send commands to all the other service endpoints in the AI system.
    
    A command has been added that commands the produce service to begin producing data to the the `data.raw` Kafka topic.

---
+++ service-orchestrate/orchestrate/produce.py
@@ -0,0 +1,55 @@
+"""Send commands to the Produce service.
+"""
+
+from datetime import datetime
+
+import pytz
+from faust import App
+from faust.cli import option
+import orchestrate.command_models.produce as cmd_mods  # pylint: disable=E0401
+
+
+app = App(
+    id="orch-produce",
+    version=1,
+    broker="aiokafka://localhost:29092",  # todo: Must modify for container
+    serializer="json",
+)
+
+produce_commands_topic = app.topic("commands.produce")
+
+
+@app.command(
+    option(
+        "--interval",
+        type=int,
+        default=5,
+        help="Add delay of N seconds between messages.",
+    ),
+    option(
+        "--max-messages",
+        type=int,
+        default=0,
+        help="Send at most N messages or 0 for infinity.",
+    ),
+)
+async def start(self, interval: int, max_messages: int) -> None:
+    """Command Produce service to begin producing to `data.raw` topic."""
+    # Initialize command from command models
+    command = cmd_mods.StartProducingCommand(
+        name=self.__name__, interval=interval, max_messages=max_messages
+    )
+    # Produce command to Kafka topic as JSON message
+    timestamp = datetime.now(pytz.UTC).isoformat()
+    await produce_commands_topic.send(
+        value=command.model_dump(),
+        headers=[
+            ("source", app.conf.id),
+            ("timestamp", bytes(timestamp, encoding="utf-8")),
+        ],
+    )
+    app.logger.info(
+        "<%s> command sent to <%s> topic successfully!",
+        self.__name__,
+        produce_commands_topic.get_topic_name(),
+    )
